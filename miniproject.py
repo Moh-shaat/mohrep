974c8c5d9ff4095711dc5af9b96613ea90f493bd
Moh-shaat <94184187+Moh-shaat@users.noreply.github.com>
2021-11-12 15:56:03 +0000

    Add files via upload

---
+++ miniproject.py
@@ -0,0 +1,12 @@
+# ex 1
+
+x=5;
+for i in range(x):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(x,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
